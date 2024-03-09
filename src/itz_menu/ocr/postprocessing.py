b332d4719dfa369084185f7ca8899a4eb46d9775
Louke <git@jannsen.org>
2024-03-10 00:41:57 +0100

    Create function to convert menu dataframe to an orm

--- src/itz_menu/ocr/postprocessing.py
+++ src/itz_menu/ocr/postprocessing.py
@@ -1,6 +1,23 @@
+from itz_menu.persistence.models import WeekMenu, DayMenu, MealCategory, Meal
+from itz_menu.persistence.enums import DietType
 import pandas as pd
 
 
-def dataframe_to_week_menu(df: pd.DataFrame):
-    print(df.to_string())
-    pass
+def dataframe_to_week_menu(df: pd.DataFrame, validity_period: tuple[int, int], filename: str) -> WeekMenu:
+    start_timestamp, end_timestamp = validity_period
+    menus = __dataframe_to_menus(df)
+    return WeekMenu(start_date=start_timestamp, end_date=end_timestamp, filename=filename, menus=menus)
+
+
+def __dataframe_to_menus(df: pd.DataFrame) -> list[DayMenu]:
+    return [DayMenu(name=day, categories=__dataframe_to_categories(df[day])) for day in df.columns if day is not None]
+
+
+def __dataframe_to_categories(s: pd.Series):
+    def merge_rows(grouped_s: pd.Series):
+        return MealCategory(name=grouped_s.index[0], meals=__dataframe_to_meals(grouped_s))
+    return s.groupby(s.index).apply(merge_rows).to_list()
+
+
+def __dataframe_to_meals(s: pd.Series) -> list[Meal]:
+    return [Meal(name=s.iloc[i], diet_type=[DietType.UNKNOWN], price=s.iloc[i + 1]) for i in range(0, len(s), 2)]
--- tests/test_itz_menu/ocr/test_postprocessing.py
+++ tests/test_itz_menu/ocr/test_postprocessing.py
@@ -1,17 +1,59 @@
 import pandas as pd
 import pytest
 
+import itz_menu.persistence.database as database
+import itz_menu.ocr.postprocessing as postprocessing
+
 from itz_menu.persistence.models import *
 
 
 @pytest.fixture()
 def week_menu() -> pd.DataFrame:
-    df = pd.read_csv('resources/speiseplanWeek.csv')
-    df.columns = df.iloc[0].map(lambda x: WeekDay.find_by_value(x), na_action='ignore')
+    df = pd.read_csv('tests/resources/speiseplanWeek.csv')
+    df.columns = df.columns.map(lambda x: WeekDay.find_by_value(x), na_action='ignore')
+    df.set_index(df.columns[0], inplace=True)
     return df
+
+
+@pytest.fixture(scope='session')
+def init_database():
+    database.init()
 
 
 class TestPostprocessing:
 
-    def test_dataframe_to_week_menu(self, week_menu: pd.DataFrame):
-        print(week_menu.index)
+    def test_dataframe_to_week_menu_validity_period(self, week_menu: pd.DataFrame, init_database):
+        validity_period = (1708297200, 1708729199)
+        menu = postprocessing.dataframe_to_week_menu(week_menu, validity_period, 'checksum.jpg')
+        assert menu.start_date == validity_period[0]
+        assert menu.end_date == validity_period[1]
+
+    def test_dataframe_to_week_menu_filename(self, week_menu: pd.DataFrame, init_database):
+        filename = 'checksum.jpg'
+        menu = postprocessing.dataframe_to_week_menu(week_menu, (0, 1), filename)
+        assert menu.filename == filename
+
+    def test_dataframe_to_week_menu_menus(self, week_menu: pd.DataFrame, init_database):
+        menu = postprocessing.dataframe_to_week_menu(week_menu, (0, 1), 'checksum.jpg')
+        assert len(menu.menus) == 5
+        assert all(isinstance(day, DayMenu) for day in menu.menus)
+
+    def test_dataframe_to_week_menu_day_menu(self, week_menu: pd.DataFrame, init_database):
+        menu = postprocessing.dataframe_to_week_menu(week_menu, (0, 1), 'checksum.jpg')
+        assert all(day.name in WeekDay for day in menu.menus)
+        assert [day.name for day in menu.menus] == WeekDay.values()
+        assert all(len(day.categories) == 5 for day in menu.menus)
+        assert all(isinstance(category, MealCategory) for day in menu.menus for category in day.categories)
+
+    def test_dataframe_to_week_menu_meal_category(self, week_menu: pd.DataFrame, init_database):
+        menu = postprocessing.dataframe_to_week_menu(week_menu, (0, 1), 'checksum.jpg')
+        categories = ('Suppe', 'Heimatküche', 'From the Bowl', 'Front Cooking', 'Naschglück')
+        assert all(category.name in categories for day in menu.menus for category in day.categories)
+        assert all(len(category.meals) in (1, 2) for day in menu.menus for category in day.categories)
+        assert all(isinstance(meal, Meal) for d in menu.menus for category in d.categories for meal in category.meals)
+
+    def test_dataframe_to_week_menu_meal(self, week_menu: pd.DataFrame, init_database):
+        menu = postprocessing.dataframe_to_week_menu(week_menu, (0, 1), 'checksum.jpg')
+        assert all(len(meal.name) > 5 for d in menu.menus for category in d.categories for meal in category.meals)
+        assert all(meal.price >= 0 for d in menu.menus for category in d.categories for meal in category.meals)
+        assert all(isinstance(meal.diet_type, list) for d in menu.menus for c in d.categories for meal in c.meals)
