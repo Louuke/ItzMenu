9d023218512b79b55b5c71ca59526a0c17466461
Louke <git@jannsen.org>
2024-03-10 00:02:39 +0100

    Automatically use test database if running unittests

--- src/itz_menu/persistence/database.py
+++ src/itz_menu/persistence/database.py
@@ -1,3 +1,5 @@
+import sys
+
 from bunnet import init_bunnet
 from pymongo import MongoClient
 
@@ -8,4 +10,9 @@ from itz_menu.persistence.models import *
 def init():
     settings = Settings()
     client = MongoClient(settings.mongo_db_url)
-    init_bunnet(database=client[settings.mongo_db_name], document_models=[WeekMenu])
+    database_name = settings.mongo_db_test_name if is_test_running() else settings.mongo_db_name
+    init_bunnet(database=client[database_name], document_models=[WeekMenu])
+
+
+def is_test_running():
+    return 'pytest' in sys.modules or 'unittest' in sys.modules
