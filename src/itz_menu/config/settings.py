3b891fe012ef98dcfd84489c35b748959524dd41
Louke <git@jannsen.org>
2024-03-10 00:02:18 +0100

    Add test database to settings file

--- src/itz_menu/config/settings.py
+++ src/itz_menu/config/settings.py
@@ -5,5 +5,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     mongo_db_url: str = Field(default='mongodb://localhost:27017', pattern=r'^mongodb(.+srv)?://.*')
     mongo_db_name: str = Field(default='development', min_length=3)
+    mongo_db_test_name: str = Field(default='test', min_length=3)
 
     model_config = SettingsConfigDict(env_file='settings.env', env_file_encoding='utf-8')
