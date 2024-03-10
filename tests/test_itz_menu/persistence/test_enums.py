bd8a85f3f63f80c04ab4b7488b84515f45f470b9
Louke <git@jannsen.org>
2024-03-10 13:32:24 +0100

    Add TestWeekDay class

---
+++ tests/test_itz_menu/persistence/test_enums.py
@@ -0,0 +1,20 @@
+from itz_menu.persistence.enums import WeekDay
+
+
+class TestWeekDay:
+
+    def test_week_day_values(self):
+        values = (WeekDay.MONDAY, WeekDay.TUESDAY, WeekDay.WEDNESDAY, WeekDay.THURSDAY, WeekDay.FRIDAY)
+        assert WeekDay.values() == values
+
+    def test_week_day_find_by_value(self):
+        assert WeekDay.find_by_value('monday') == WeekDay.MONDAY
+        assert WeekDay.find_by_value('Montag') == WeekDay.MONDAY
+        assert WeekDay.find_by_value('tuesday') == WeekDay.TUESDAY
+        assert WeekDay.find_by_value('Dienstag') == WeekDay.TUESDAY
+        assert WeekDay.find_by_value('wednesday') == WeekDay.WEDNESDAY
+        assert WeekDay.find_by_value('Mittwoch') == WeekDay.WEDNESDAY
+        assert WeekDay.find_by_value('thursday') == WeekDay.THURSDAY
+        assert WeekDay.find_by_value('Donnerstag') == WeekDay.THURSDAY
+        assert WeekDay.find_by_value('friday') == WeekDay.FRIDAY
+        assert WeekDay.find_by_value('Freitag') == WeekDay.FRIDAY
