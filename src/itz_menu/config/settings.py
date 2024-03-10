1760cb9ebd28d2782b057052e85361a14ebc61d5
Louke <git@jannsen.org>
2024-03-10 16:10:48 +0100

    Add menu check interval to settings

--- src/itz_menu/config/settings.py
+++ src/itz_menu/config/settings.py
@@ -6,5 +6,6 @@ class Settings(BaseSettings):
     mongo_db_url: str = Field(default='mongodb://localhost:27017', pattern=r'^mongodb(.+srv)?://.*')
     mongo_db_name: str = Field(default='development', min_length=3)
     mongo_db_test_name: str = Field(default='test', min_length=3)
+    menu_check_interval: int = Field(default=3600, ge=1)
 
     model_config = SettingsConfigDict(env_file='settings.env', env_file_encoding='utf-8')
--- tests/test_itz_menu/config/test_settings.py
+++ tests/test_itz_menu/config/test_settings.py
@@ -27,3 +27,13 @@ class TestSettings:
     def test_settings_with_invalid_mongo_db_name():
         with pytest.raises(ValidationError):
             Settings(mongo_db_url='mongodb://localhost:27017', mongo_db_name='a')
+
+    @staticmethod
+    def test_settings_with_valid_menu_check_interval():
+        settings = Settings(menu_check_interval=3600)
+        assert settings.menu_check_interval == 3600
+
+    @staticmethod
+    def test_settings_with_invalid_menu_check_interval():
+        with pytest.raises(ValidationError):
+            Settings(menu_check_interval=0)
