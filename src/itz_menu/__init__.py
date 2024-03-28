e01a0c4a17b3309fdc79811029d3e7fefde17992
Louke <git@jannsen.org>
2024-03-28 23:58:16 +0100

    Use AsyncIOScheduler to schedule week menu job

--- src/itz_menu/__init__.py
+++ src/itz_menu/__init__.py
@@ -6,4 +6,3 @@ from itz_menu.config.settings import Settings
 __version__ = '0.0.1'
 log.basicConfig(level=log.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 log.info(f'Running with settings: {Settings()}')
-database.init()
--- src/itz_menu/__main__.py
+++ src/itz_menu/__main__.py
@@ -1,19 +1,35 @@
+import asyncio
+import datetime
+import os
 import sys
 import re
 import logging as log
 
-from itz_menu.services import UpdateMenuService
-from itz_menu.utils import load_image
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+import itz_menu.jobs as jobs
+import itz_menu.utils as utils
+from itz_menu.persistence import database
 
 
-service = UpdateMenuService()
+async def main():
+    await database.init()
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(jobs.fetch_menu, 'interval', seconds=3600, max_instances=1, next_run_time=datetime.datetime.now())
+    scheduler.start()
+    log.info('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+    while True:
+        await asyncio.sleep(1000)
 
 if len(sys.argv) == 1:
-    service.start()
-    service.join()
+    # Execution will block here until Ctrl+C (Ctrl+Break on Windows) is pressed.
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        pass
 elif all(re.match(r'^[a-zA-Z0-9/_-]+\.jpg$', filename) for filename in sys.argv[1:]):
-    images = [load_image(filename) for filename in sys.argv[1:]]
-    [service.process_image(image) for image in images]
+    images = [utils.load_image(filename) for filename in sys.argv[1:]]
+    [jobs.process_image(image) for image in images]
 else:
     log.warning(f'Invalid arguments: {sys.argv}')
     log.warning('Usage: itz_menu [filename.jpg ...]')
