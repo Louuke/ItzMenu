d8d84608c5be58a8277361a150e952a98dd48510
Louke <git@jannsen.org>
2024-05-01 21:24:22 +0200

    Validate received image using checksum

--- tests/test_itzmenu/rest/test_client.py
+++ tests/test_itzmenu/rest/test_client.py
@@ -1,4 +1,5 @@
 from itzmenu.rest.client import MenuClient
+from itzmenu.utils import bytes_to_sha256
 
 
 class TestClient:
@@ -8,6 +9,7 @@ class TestClient:
         image = menu_client.get_week_menu()
         assert isinstance(image, bytes)
         assert len(image) > 0
+        assert bytes_to_sha256(image) == 'c0f2190a1b6536ff868fe2436993e25814c763c98db8068ca4c27ce78dfb68a6'
 
     def test_menu_client_get_week_menu_failure(self, httpserver):
         menu_client = MenuClient('http://127.0.0.1:8081')
