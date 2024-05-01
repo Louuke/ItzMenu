ccd14ec423a3f3d7590e6801907fd98a36eb1272
Louke <git@jannsen.org>
2024-05-01 22:06:13 +0200

    Check if images are cropped correctly

--- tests/test_itzmenu/ocr/test_preprocess.py
+++ tests/test_itzmenu/ocr/test_preprocess.py
@@ -30,3 +30,20 @@ class TestPreprocess:
         white_range = range(255 - tolerance, 256)
         assert all(color in black_range or color in white_range for color in unique_colors)
 
+    def test_crop_main_image_content(self, week_menu: bytes):
+        @preprocess.crop_main_image_content
+        def dummy_func(img: bytes) -> bytes:
+            return img
+
+        pimage_org = Image.open(io.BytesIO(week_menu))
+        with_org, height_org = pimage_org.size
+
+        # Apply the crop_main_image_content decorator through the dummy function
+        image = dummy_func(week_menu)
+
+        pimage_cropped = Image.open(io.BytesIO(image))
+        with_cropped, height_cropped = pimage_cropped.size
+
+        # Check if the image is cropped correctly
+        assert with_org > with_cropped
+        assert height_org > height_cropped
