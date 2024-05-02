8c9518e595b17f28a04970ae2ae0cddfdb163b9b
Louke <git@jannsen.org>
2024-05-02 19:39:13 +0200

    Test caching of img_to_dataframe function

--- tests/test_itzmenu/ocr/test_extractor.py
+++ tests/test_itzmenu/ocr/test_extractor.py
@@ -1,32 +1,27 @@
 import re
 import time
 
-import pandas as pd
-import pytest
-
 import itzmenu.ocr.extractor as extractor
 from itzmenu.persistence.enums import WeekDay
-
-
-@pytest.fixture(scope='class')
-def df(week_menu: bytes) -> pd.DataFrame:
-    return extractor.img_to_dataframe(week_menu)
 
 
 class TestExtractor:
 
     @staticmethod
-    def test_as_data_frame_columns_correct(df: pd.DataFrame):
+    def test_img_to_dataframe_column_correct(week_menu: bytes):
+        df = extractor.img_to_dataframe(week_menu)
         assert ([WeekDay.MONDAY, WeekDay.TUESDAY, WeekDay.WEDNESDAY, WeekDay.THURSDAY, WeekDay.FRIDAY]
                 == df.columns.tolist())
 
     @staticmethod
-    def test_as_data_frame_index_correct(df: pd.DataFrame):
+    def test_img_to_dataframe_index_correct(week_menu: bytes):
+        df = extractor.img_to_dataframe(week_menu)
         assert df.index.tolist() == ['Suppe', 'Heimatküche', 'From the Bowl', 'Front Cooking', 'Naschglück',
                                      'Naschglück']
 
     @staticmethod
-    def test_every_row_contains_price_at_end(df: pd.DataFrame):
+    def test_img_to_dataframe_prices_at_end(week_menu: bytes):
+        df = extractor.img_to_dataframe(week_menu)
         # Check if each cell end with a price
         has_price = df.map(lambda x: re.search(r'\d+.\d+$', x) is not None)
         # Check if all values in each row are True
@@ -35,15 +30,26 @@ class TestExtractor:
         assert all_have_prices.all(), "Not all rows contain a price at the end."
 
     @staticmethod
-    def test_every_second_row_contains_description(df: pd.DataFrame):
-        # Select every second row
-        df_second_rows = df.iloc[::2]
-        # Check if each cell contains a string
-        is_string = df_second_rows.map(lambda x: isinstance(x, str))
-        # Check if all values in each row are True
-        all_strings = is_string.all(axis=1)
-        # Assert that all values are True
-        assert all_strings.all(), "Not all values in every second row are strings."
+    def test_img_to_dataframe_cache_one(week_menu: bytes):
+        df = extractor.img_to_dataframe(week_menu)
+        start = time.time()
+        df2 = extractor.img_to_dataframe(week_menu)
+        stop = time.time()
+        assert df.equals(df2)
+        assert stop - start < 1
+
+    @staticmethod
+    def test_img_to_dataframe_cache_multiple(week_menu: bytes, week_menu_holiday: bytes):
+        df1 = extractor.img_to_dataframe(week_menu)
+        df2 = extractor.img_to_dataframe(week_menu_holiday)
+        start = time.time()
+        df3 = extractor.img_to_dataframe(week_menu)
+        df4 = extractor.img_to_dataframe(week_menu_holiday)
+        stop = time.time()
+        assert df1.equals(df3)
+        assert df2.equals(df4)
+        assert not df3.equals(df4)
+        assert stop - start < 1
 
     @staticmethod
     def test_period_of_validity_timestamp(week_menu: bytes):
