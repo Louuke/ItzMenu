6a07b805cf13ba322f6b653380ae2062a39cebf4
Louke <git@jannsen.org>
2024-05-09 01:03:41 +0200

    Use ArgumentParser to start service

--- itzmenu-service/src/itzmenu_service/__main__.py
+++ itzmenu-service/src/itzmenu_service/__main__.py
@@ -1,5 +1,14 @@
 import uvicorn
+import argparse
 
 
 def main():
-    uvicorn.run('itzmenu_service.app:app', host='0.0.0.0', log_level='debug')
+    parser = argparse.ArgumentParser(description='ItzMenu Service')
+    parser.add_argument('-port',  '-p', type=int, default=8000, help='Port to run the service on')
+    parser.add_argument('-host',  '-ht', type=str, default='127.0.0.1', help='Host to run the service on')
+    args = parser.parse_args()
+    uvicorn.run('itzmenu_service.app:app', host=args.host, port=args.port, log_level='debug')
+
+
+if __name__ == '__main__':
+    main()
