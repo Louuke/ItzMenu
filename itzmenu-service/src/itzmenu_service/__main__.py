c353772829d773e9b6e5131a1a4ce3bad9628dcc
Louke <git@jannsen.org>
2024-05-09 01:29:59 +0200

    Control log level using command argument

--- itzmenu-service/src/itzmenu_service/__main__.py
+++ itzmenu-service/src/itzmenu_service/__main__.py
@@ -4,10 +4,11 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='ItzMenu Service')
-    parser.add_argument('-port',  '-p', type=int, default=8000, help='Port to run the service on')
-    parser.add_argument('-host',  '-ht', type=str, default='127.0.0.1', help='Host to run the service on')
+    parser.add_argument('-port',  '-p', type=int, default=8000, help='port to run the service on')
+    parser.add_argument('-host',  '-ht', type=str, default='127.0.0.1', help='host to run the service on')
+    parser.add_argument('-log',  '-l', type=str, default='info', help='log level')
     args = parser.parse_args()
-    uvicorn.run('itzmenu_service.app:app', host=args.host, port=args.port, log_level='debug')
+    uvicorn.run('itzmenu_service.app:app', host=args.host, port=args.port, log_level=args.log)
 
 
 if __name__ == '__main__':
