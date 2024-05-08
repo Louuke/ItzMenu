0cf864712c88544dda3d0ea9ca1a112709889d19
Louke <git@jannsen.org>
2024-05-08 23:03:23 +0200

    Remove old tests

--- itzmenu-service/tests/test_itzmenu_service/test_app.py
+++ itzmenu-service/tests/test_itzmenu_service/test_app.py
@@ -46,15 +46,3 @@ class TestAppAuth:
             response = await http_client.post('/auth/forgot-password', json=data)
             assert response.status_code == 202
             assert len(srv.messages) == 1
-
-
-@pytest.mark.asyncio
-async def test_read_main(user_manager):
-    user = await user_manager.get_by_email('fastapi@jannsen.org')
-    print(user)
-
-
-@pytest.mark.asyncio
-async def test_read_main2(http_client):
-    response = await http_client.post('/auth/login', data={"username": "fastapi@jannsen.org", "password": "password"})
-    print(response.json())
