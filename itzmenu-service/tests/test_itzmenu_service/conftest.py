60801610c9fd63066fa172ee4a745c152f3c1078
Louke <git@jannsen.org>
2024-05-09 14:29:43 +0200

    Remove unused code

--- itzmenu-service/tests/test_itzmenu_service/conftest.py
+++ itzmenu-service/tests/test_itzmenu_service/conftest.py
@@ -20,13 +20,6 @@ def override_settings():
     os.environ['mail_smtp_skip_login'] = 'true'
 
 
-# @pytest.fixture(scope='session', autouse=True)
-# def event_loop(request):
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
-
-
 @pytest_asyncio.fixture(scope='module', autouse=True)
 async def prepare_database():
     async with app.lifespan():
