f429f0364892b2093c05bebadd4e6199c814186b
Louke <git@jannsen.org>
2024-05-11 02:32:48 +0200

    User model uses UUID as id field type

--- itzmenu-service/src/itzmenu_service/persistence/models.py
+++ itzmenu-service/src/itzmenu_service/persistence/models.py
@@ -1,8 +1,12 @@
+from uuid import UUID, uuid4
+
 from beanie import Document
 from fastapi_users.db import BeanieBaseUser
+from pydantic import Field
 
 
 class User(BeanieBaseUser, Document):
+    id: UUID = Field(default_factory=uuid4)
     permissions: set[str] = []
 
     class Settings(BeanieBaseUser.Settings):
