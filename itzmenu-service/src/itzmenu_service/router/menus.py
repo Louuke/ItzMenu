eff490c70f2948c0f74dc042e739394183ffc3ee
Louke <git@jannsen.org>
2024-05-22 19:38:57 +0200

    Rename menu manager parameter

--- itzmenu-service/src/itzmenu_service/router/menus.py
+++ itzmenu-service/src/itzmenu_service/router/menus.py
@@ -41,9 +41,9 @@ def get_menus_router(get_week_menu_manager: WeekMenuManagerDependency[ID],
                      },
                  })
     async def create_menu(request: Request, user_create: menu_create_schema,
-                          user_manager: BaseWeekMenuManager[ID] = Depends(get_week_menu_manager)):
+                          menu_manager: BaseWeekMenuManager[ID] = Depends(get_week_menu_manager)):
         try:
-            created_user = await user_manager.create(user_create, request=request)
+            created_user = await menu_manager.create(user_create, request=request)
         except exceptions.WeekMenuAlreadyExists as e:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail=ErrorCode.CREATE_MENU_ALREADY_EXISTS) from e
@@ -61,12 +61,11 @@ def get_menus_router(get_week_menu_manager: WeekMenuManagerDependency[ID],
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=ErrorCode.GET_MENU_NOT_FOUND) from e
 
-    @router.get('/', response_model=menu_read_schema, name='menus:filter_menu')
-    async def filter_menu(user_manager: BaseWeekMenuManager[ID] = Depends(get_week_menu_manager),
-                          timestamp: Optional[int] = time.time()):
+    @router.get('/', response_model=menu_read_schema, name='menus:get_menu_by_timestamp')
+    async def get_menu_by_timestamp(menu_manager: BaseWeekMenuManager[ID] = Depends(get_week_menu_manager),
+                                    timestamp: Optional[int] = time.time()):
         try:
-            menu = await user_manager.get_by_timestamp(timestamp)
-            return menu
+            return await menu_manager.get_by_timestamp(timestamp)
         except exceptions.WeekMenuNotExists as e:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=ErrorCode.GET_MENU_NOT_FOUND) from e
