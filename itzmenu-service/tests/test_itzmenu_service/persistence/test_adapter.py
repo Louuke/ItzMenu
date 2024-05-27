e28ddbdcc2fd25dea78ab335f635295bea6fad23
Louke <git@jannsen.org>
2024-05-27 21:41:42 +0200

    Additional save week menu tests

--- itzmenu-service/tests/test_itzmenu_service/persistence/test_adapter.py
+++ itzmenu-service/tests/test_itzmenu_service/persistence/test_adapter.py
@@ -1,4 +1,7 @@
+import time
+
 import pytest
+from pymongo.errors import DuplicateKeyError
 
 from itzmenu_service.persistence.adapter.adapter import BeanieWeekMenuDatabase
 from itzmenu_api.persistence.schemas import WeekMenuCreate, DayMenu, WeekDay, MealCategory, Meal
@@ -12,11 +15,11 @@ class TestBaseWeekMenuDatabase:
         meal = Meal(name='test', price=1.0)
         category = MealCategory(name='test', meals=[meal])
         day = DayMenu(name=WeekDay.MONDAY, categories=[category])
-        menu = WeekMenuCreate(filename='test.jpg', start_timestamp=1, end_timestamp=5, menus=[day])
+        menu = WeekMenuCreate(filename='test1.jpg', start_timestamp=1, end_timestamp=5, menus=[day])
         create = menu.create_update_dict()
         result = await menu_db.create(create)
         assert result.id is not None
-        assert result.filename == 'test.jpg'
+        assert result.filename == 'test1.jpg'
         assert result.start_timestamp == 1
         assert result.end_timestamp == 5
         assert len(result.menus) == 1
@@ -25,3 +28,21 @@ class TestBaseWeekMenuDatabase:
         assert result.menus[0].categories[0].name == 'test'
         assert len(result.menus[0].categories[0].meals) == 1
         assert result.menus[0].categories[0].meals[0].name == 'test'
+
+    @pytest.mark.dependency(depends=['TestBaseWeekMenuDatabase::test_create_success'])
+    async def test_create_duplicate(self, menu_db: BeanieWeekMenuDatabase):
+        menu = WeekMenuCreate(filename='test1.jpg', start_timestamp=1, end_timestamp=5)
+        create = menu.create_update_dict()
+        with pytest.raises(DuplicateKeyError):
+            await menu_db.create(create)
+
+    async def test_create_missing_fields(self, menu_db: BeanieWeekMenuDatabase):
+        menu = WeekMenuCreate(filename='test2.jpg', start_timestamp=2, end_timestamp=10)
+        create = menu.create_update_dict()
+        result = await menu_db.create(create)
+        assert result.id is not None
+        assert result.filename == 'test2.jpg'
+        assert result.start_timestamp == 2
+        assert result.end_timestamp == 10
+        assert len(result.menus) == 0
+        assert result.created_at > time.time() - 5
