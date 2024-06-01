0542b99e06da1a554f9167cd7e3ce48597152909
Louke <git@jannsen.org>
2024-06-02 00:17:04 +0200

    Create test users with session scope

--- itzmenu-service/tests/test_itzmenu_service/conftest.py
+++ itzmenu-service/tests/test_itzmenu_service/conftest.py
@@ -4,10 +4,13 @@ from uuid import UUID
 import pytest
 import pytest_asyncio
 from fastapi_users import BaseUserManager
+from fastapi_users.jwt import generate_jwt
+from fastapi_users.password import PasswordHelper
 from fastapi_users_db_beanie import BeanieUserDatabase
 from httpx import AsyncClient, ASGITransport
 
 from itzmenu_service import app
+from itzmenu_service.config.settings import Settings
 from itzmenu_service.manager.users import get_user_manager
 from itzmenu_service.manager.menus import get_week_menu_manager
 from itzmenu_service.persistence.adapter.adapter import BeanieWeekMenuDatabase
@@ -29,6 +32,66 @@ async def prepare_database():
     async with app.lifespan():
         await User.find().delete()
         await WeekMenu.find().delete()
+
+
+@pytest.fixture(scope='session')
+def user_pwd() -> str:
+    return 'paSSw0rd'
+
+
+@pytest.fixture(scope='session')
+def user_hashed_pwd(user_pwd: str) -> str:
+    return PasswordHelper().hash(user_pwd)
+
+
+@pytest_asyncio.fixture(scope='session')
+async def user_w_permissions(user_hashed_pwd: str) -> User:
+    email = 'user_w_permissions@example.org'
+    permissions = ['menus:create_menu', 'menus:update_menu', 'menus:delete_menu']
+    return await User(email=email, hashed_password=user_hashed_pwd, permissions=permissions).create()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def user_wo_permissions(user_hashed_pwd: str) -> User:
+    email = 'user_wo_permissions@example.org'
+    return await User(email=email, hashed_password=user_hashed_pwd).create()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def user_superuser(user_hashed_pwd: str) -> User:
+    email = 'superuser@example.org'
+    return await User(email=email, hashed_password=user_hashed_pwd, is_superuser=True).create()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def user_inactive(user_hashed_pwd: str) -> User:
+    email = 'user_inactive@example.org'
+    user = await User(email=email, hashed_password=user_hashed_pwd).create()
+    user.is_active = False
+    await user.save()
+    return await User.find_one(User.id == user.id)
+
+
+@pytest_asyncio.fixture(scope='session')
+async def user_w_permissions_headers(user_w_permissions: User) -> dict[str, str]:
+    return __create_headers(user_w_permissions)
+
+
+@pytest_asyncio.fixture(scope='session')
+async def user_wo_permissions_headers(user_wo_permissions: User) -> dict[str, str]:
+    return __create_headers(user_wo_permissions)
+
+
+@pytest_asyncio.fixture(scope='session')
+async def user_superuser_headers(user_superuser: User) -> dict[str, str]:
+    return __create_headers(user_superuser)
+
+
+def __create_headers(user: User) -> dict[str, str]:
+    aud = ['fastapi-users:auth'] + list(user.permissions) + (['*:*'] if user.is_superuser else [])
+    data = {'sub': str(user.id), 'aud': aud}
+    token = generate_jwt(data, Settings().service_secret, 3600, algorithm='HS256')
+    return {'Authorization': f'Bearer {token}'}
 
 
 @pytest.fixture
