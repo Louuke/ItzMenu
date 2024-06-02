bff7ea0c78eacaf5835d0605350d197b31998fd9
Louke <git@jannsen.org>
2024-06-02 21:28:00 +0200

    Remove mongo_db_test_name attribute

--- itzmenu-service/src/itzmenu_service/config/settings.py
+++ itzmenu-service/src/itzmenu_service/config/settings.py
@@ -6,7 +6,6 @@ import re
 class Settings(BaseSettings):
     mongo_db_url: str = Field(default='mongodb://localhost:27017', pattern=r'^mongodb(.+srv)?://.*')
     mongo_db_name: str = Field(default='development', min_length=3)
-    mongo_db_test_name: str = Field(default='test', min_length=3)
     mail_smtp_host: str = Field(default='localhost')
     mail_smtp_port: int = Field(default=587, ge=1, le=65535)
     mail_smtp_user: str = Field(default='user')
@@ -20,8 +19,7 @@ class Settings(BaseSettings):
 
     def __str__(self):
         truncated_url = re.sub(r'(?<=://).+@', 'XXX:XXX@', self.mongo_db_url)
-        return f'mongo_db_url={truncated_url}, mongo_db_name={self.mongo_db_name}, ' \
-               f'mongo_db_test_name={self.mongo_db_test_name}'
+        return f'mongo_db_url={truncated_url}, mongo_db_name={self.mongo_db_name}'
 
 
 settings = Settings()
