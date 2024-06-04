d5b8ec84f994650abafe20dd4642751f95e72e3d
Louke <git@jannsen.org>
2024-06-04 21:26:51 +0200

    Trying get_menu_by_timestamp method using end_timestamp value

--- itzmenu-client/tests/test_itzmenu_client/client.py
+++ itzmenu-client/tests/test_itzmenu_client/client.py
@@ -37,14 +37,24 @@ def test_get_menu_by_id(httpserver: HTTPServer, week_menus: list[dict[str, Any]]
 
 
 def test_get_menu_by_timestamp(httpserver: HTTPServer, week_menus: list[dict[str, Any]]):
-    resp = week_menus[0]
-    (httpserver.expect_request('/menus/menu', method='GET', query_string=f'timestamp={resp["start_timestamp"] + 1}')
-     .respond_with_json(resp))
+    resp1 = week_menus[0]
+    (httpserver.expect_request('/menus/menu', method='GET', query_string=f'timestamp={resp1["start_timestamp"] + 1}')
+     .respond_with_json(resp1))
     client = ItzMenuClient('user', 'password', f'http://{httpserver.host}:{httpserver.port}')
-    response = client.get_menu_by_timestamp(resp['start_timestamp'] + 1)
-    assert response.start_timestamp == resp['start_timestamp']
-    assert response.end_timestamp == resp['end_timestamp']
-    assert response.created_at == resp['created_at']
-    assert response.filename == resp['filename']
-    assert response.id == UUID(resp['id'])
+    response = client.get_menu_by_timestamp(resp1['start_timestamp'] + 1)
+    assert response.start_timestamp == resp1['start_timestamp']
+    assert response.end_timestamp == resp1['end_timestamp']
+    assert response.created_at == resp1['created_at']
+    assert response.filename == resp1['filename']
+    assert response.id == UUID(resp1['id'])
+    assert response.menus == []
+    resp2 = week_menus[1]
+    (httpserver.expect_request('/menus/menu', method='GET', query_string=f'timestamp={resp2["end_timestamp"]}')
+     .respond_with_json(resp2))
+    response = client.get_menu_by_timestamp(resp2['end_timestamp'])
+    assert response.start_timestamp == resp2['start_timestamp']
+    assert response.end_timestamp == resp2['end_timestamp']
+    assert response.created_at == resp2['created_at']
+    assert response.filename == resp2['filename']
+    assert response.id == UUID(resp2['id'])
     assert response.menus == []
