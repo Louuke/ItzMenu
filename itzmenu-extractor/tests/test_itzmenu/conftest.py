895e0f1c8ec54d4077a685d51e5f24dce9e88ef8
Louke <git@jannsen.org>
2024-06-15 16:44:13 +0200

    Remove no longer needed fixtures

--- itzmenu-extractor/tests/test_itzmenu/conftest.py
+++ itzmenu-extractor/tests/test_itzmenu/conftest.py
@@ -1,18 +1,12 @@
-import asyncio
-import importlib.resources as pkg_resources
+from pathlib import Path
+
 import pytest
-
-import itzmenu_api.persistence.database as database
-
-
-@pytest.fixture(scope='session')
-def httpserver_listen_address():
-    return '127.0.0.1', 8080
+from pytest_httpserver import HTTPServer
 
 
 @pytest.fixture()
 def httpserver(make_httpserver, week_menu: bytes):
-    server = make_httpserver
+    server: HTTPServer = make_httpserver
     server.expect_request('/media/img/speiseplanWeek.jpg', method='GET').respond_with_data(week_menu)
     yield server
     server.clear()
@@ -20,20 +14,9 @@ def httpserver(make_httpserver, week_menu: bytes):
 
 @pytest.fixture(scope='session')
 def week_menu() -> bytes:
-    return pkg_resources.files('resources').joinpath('speiseplanWeek.jpg').read_bytes()
+    return (Path(__file__).parents[1] / 'resources' / 'speiseplanWeek.jpg').read_bytes()
 
 
 @pytest.fixture(scope='session')
 def week_menu_holiday() -> bytes:
-    return pkg_resources.files('resources').joinpath('speiseplanWeekHoliday.jpg').read_bytes()
-
-
-@pytest.fixture(scope='session')
-def event_loop():
-    return asyncio.get_event_loop()
-
-
-@pytest.fixture(scope='session')
-def init_database(event_loop):
-    event_loop.run_until_complete(database.init())
-
+    return (Path(__file__).parents[1] / 'resources' / 'speiseplanWeekHoliday.jpg').read_bytes()
