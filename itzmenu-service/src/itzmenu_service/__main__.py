fbc2c8365461b3db5d9d607687390527160d3873
Louke <git@jannsen.org>
2024-06-29 16:34:13 +0200

    Parse cli arguments using pydantic_settings

--- itzmenu-service/src/itzmenu_service/__main__.py
+++ itzmenu-service/src/itzmenu_service/__main__.py
@@ -1,14 +1,21 @@
 import uvicorn
 import argparse
 
+from itzmenu_service.config.settings import Settings
+
 
 def main():
     parser = argparse.ArgumentParser(description='ItzMenu Service')
-    parser.add_argument('-port',  '-p', type=int, default=8000, help='port to run the service on')
-    parser.add_argument('-host',  '-ht', type=str, default='127.0.0.1', help='host to run the service on')
-    parser.add_argument('-log',  '-l', type=str, default='info', help='log level')
+    for name, field in Settings.__fields__.items():
+        parser.add_argument(
+            f"--{name}",
+            dest=name,
+            type=field.annotation,
+            default=field.default,
+            help=field.description,
+        )
     args = parser.parse_args()
-    uvicorn.run('itzmenu_service.app:app', host=args.host, port=args.port, log_level=args.log)
+    uvicorn.run('itzmenu_service.app:app', host=args.service_host, port=args.service_port, log_level=args.log)
 
 
 if __name__ == '__main__':
--- itzmenu-service/src/itzmenu_service/config/settings.py
+++ itzmenu-service/src/itzmenu_service/config/settings.py
@@ -1,11 +1,16 @@
 from pydantic import Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict, PydanticBaseSettingsSource
 import re
 
 
 class Settings(BaseSettings):
-    mongo_db_url: str = Field(default='mongodb://localhost:27017', pattern=r'^mongodb(.+srv)?://.*')
-    mongo_db_name: str = Field(default='development', min_length=3)
+    service_host: str = Field(default='127.0.0.1', description='host to run the service on')
+    service_port: int = Field(default=8000, description='port to run the service on')
+    service_secret: str = Field(default='SECRET', min_length=6)
+    service_public_address: str = Field(default='http://localhost:8000')
+
+    mongodb_url: str = Field(default='mongodb://localhost:27017', pattern=r'^mongodb(.+srv)?://.*')
+    mongodb_name: str = Field(default='test', min_length=3)
     mail_smtp_host: str = Field(default='localhost')
     mail_smtp_port: int = Field(default=587, ge=1, le=65535)
     mail_smtp_user: str = Field(default='user')
@@ -13,14 +18,14 @@ class Settings(BaseSettings):
     mail_smtp_tls: bool = Field(default=True)
     mail_smtp_starttls: bool = Field(default=False)
     mail_smtp_skip_login: bool = Field(default=False)
-    service_secret: str = Field(default='SECRET', min_length=6)
-    service_host: str = Field(default='http://localhost:8000')
 
-    model_config = SettingsConfigDict(env_file='settings.env', env_file_encoding='utf-8')
+    log: str = Field(default='info', description='log level')
+
+    model_config = SettingsConfigDict(env_file='settings.env', env_file_encoding='utf-8', cli_parse_args=True)
 
     def __str__(self):
-        truncated_url = re.sub(r'(?<=://).+@', 'XXX:XXX@', self.mongo_db_url)
-        return f'mongo_db_url={truncated_url}, mongo_db_name={self.mongo_db_name}'
+        truncated_url = re.sub(r'(?<=://).+@', 'XXX:XXX@', self.mongodb_url)
+        return f'mongodb_url={truncated_url}, mongodb_name={self.mongodb_name}'
 
 
 settings = Settings()
