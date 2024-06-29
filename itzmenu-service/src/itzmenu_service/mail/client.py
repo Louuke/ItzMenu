70dda3e1a08159789d3d458d71f7311eae5d5c5b
Louke <git@jannsen.org>
2024-06-29 16:39:50 +0200

    Create SMTPClient and MailTemplate wrapper classes

--- itzmenu-service/src/itzmenu_service/mail/client.py
+++ itzmenu-service/src/itzmenu_service/mail/client.py
@@ -1,25 +1,97 @@
-from typing import Callable
+import re
+from pathlib import Path
+from typing import Any
 
 import yagmail
 
-from itzmenu_service.mail.templates import user_verification_email, user_reset_password_email
 from itzmenu_service.config.settings import Settings
 
 
-def send_email(email: str | list[str], subject: str, body_func: Callable[[str], str]):
-    settings = Settings()
-    body = body_func(settings.service_host)
-    yag = yagmail.SMTP(settings.mail_smtp_user, settings.mail_smtp_password,
-                       settings.mail_smtp_host, settings.mail_smtp_port,
-                       settings.mail_smtp_starttls, settings.mail_smtp_tls,
-                       smtp_skip_login=settings.mail_smtp_skip_login)
-    yag.send(email, subject, body)
-    yag.close()
+class MailTemplate:
+
+    def __init__(self, subject: str, template_name: str, placeholders: dict[str, Any] = None):
+        """
+        Wrapper class for email templates.
+
+        :param subject: Subject of the email
+        :param template_name: Name of the template file
+        :param placeholders: Dictionary with placeholders for the template
+        """
+        self.__subject = subject
+        self.__template_name = template_name
+        placeholders = placeholders if placeholders else {}
+        self.__placeholders = Settings().model_dump() | placeholders
+
+    def to_html(self) -> str:
+        """
+        Load the template and replace placeholders with values.
+
+        :return: HTML content of the template
+        """
+        return self.__load_and_replace()
+
+    @property
+    def subject(self) -> str:
+        """
+        Get the subject of the email.
+
+        :return: Subject of the email
+        """
+        return self.__subject
+
+    def __replacements(self) -> dict[str, Any]:
+        return {'{{ ' + key + ' }}': value for key, value in self.__placeholders.items()}
+
+    def __load_and_replace(self) -> str:
+        replacements = self.__replacements()
+        file = Path(__file__).parent / f'templates/{self.__template_name}.html'
+        with open(file, 'r') as f:
+            content = f.read()
+            pattern = re.compile("|".join(map(re.escape, replacements.keys())))
+            content = pattern.sub(lambda match: replacements[match.group(0)], content)
+        return content
 
 
-def send_verification_email(email: str, token: str):
-    send_email(email, "Verify your email", lambda host: user_verification_email(token, host))
+class SMTPClient:
 
+    def __init__(self):
+        """
+        Wrapper class for sending emails using SMTP.
+        """
+        self.__settings = Settings()
+        self.__yag = yagmail.SMTP(self.__settings.mail_smtp_user, self.__settings.mail_smtp_password,
+                                  self.__settings.mail_smtp_host, self.__settings.mail_smtp_port,
+                                  self.__settings.mail_smtp_starttls, self.__settings.mail_smtp_tls,
+                                  smtp_skip_login=self.__settings.mail_smtp_skip_login)
 
-def send_reset_password_email(email: str, token: str):
-    send_email(email, "Reset your password", lambda host: user_reset_password_email(token, host))
+    def send_email(self, email: str | list[str], template: MailTemplate):
+        """
+        Send an email using the SMTP client.
+
+        :param email: Recipient email address
+        :param template: Email template
+        :return: None
+        """
+        self.__yag.send(email, template.subject, template.to_html())
+
+    def send_verification_email(self, email: str, token: str):
+        """
+        Send a verification email.
+
+        :param email: Recipient email address
+        :param token: Verification token
+        :return: None
+        """
+        template = MailTemplate("Verify your email", "verify", {"token": token})
+        self.send_email(email, template)
+
+    def send_reset_password_email(self, email: str, token: str):
+        """
+        Send a reset password email.
+
+        :param email: Recipient email address
+        :param token: Reset password token
+        :return: None
+        """
+        template = MailTemplate("Reset your password", "reset", {"token": token})
+        self.send_email(email, template)
--- itzmenu-service/tests/test_itzmenu_service/mail/test_client.py
+++ itzmenu-service/tests/test_itzmenu_service/mail/test_client.py
@@ -3,38 +3,62 @@ import re
 
 from smtp_test_server.context import SmtpMockServer
 
-from itzmenu_service.mail.client import send_email, send_verification_email, send_reset_password_email
+from itzmenu_service.config.settings import Settings
+from itzmenu_service.mail.client import SMTPClient, MailTemplate
 
 TEST_SEND_EMAIL = 'test_send@example.org'
 
 
-def test_send_email():
-    with SmtpMockServer('127.0.0.1', 42000) as srv:
-        assert len(srv.messages) == 0
-        send_email(TEST_SEND_EMAIL, 'Test', lambda _: 'Test')
-        assert len(srv.messages) == 1
-        assert srv.messages[0]['To'] == TEST_SEND_EMAIL
+class TestSMTPClient:
+
+    def test_send_email(self):
+        client = SMTPClient()
+        with SmtpMockServer('127.0.0.1', 42000) as srv:
+            assert len(srv.messages) == 0
+            template = MailTemplate('Test', 'verify')
+            client.send_email(TEST_SEND_EMAIL, template)
+            assert len(srv.messages) == 1
+            assert srv.messages[0]['To'] == TEST_SEND_EMAIL
+
+    def test_send_verification_email(self):
+        client = SMTPClient()
+        with SmtpMockServer('127.0.0.1', 42000) as srv:
+            client.send_verification_email(TEST_SEND_EMAIL, 'token')
+            message: Message = srv.messages[0]
+            for part in message.walk():
+                if part.get_content_type() == 'text/plain':
+                    pattern = r'/auth/verify\?token=token'
+                    match = re.search(pattern, part.get_payload(decode=True).decode('utf-8'))
+                    assert match is not None
+                    break
+
+    def test_send_reset_password_email(self):
+        client = SMTPClient()
+        with SmtpMockServer('127.0.0.1', 42000) as srv:
+            client.send_reset_password_email(TEST_SEND_EMAIL, 'token')
+            message: Message = srv.messages[0]
+            for part in message.walk():
+                if part.get_content_type() == 'text/plain':
+                    pattern = r'/auth/reset-password\?token=token'
+                    match = re.search(pattern, part.get_payload(decode=True).decode('utf-8'))
+                    assert match is not None
+                    break
 
 
-def test_send_verification_email():
-    with SmtpMockServer('127.0.0.1', 42000) as srv:
-        send_verification_email(TEST_SEND_EMAIL, 'token')
-        message: Message = srv.messages[0]
-        for part in message.walk():
-            if part.get_content_type() == 'text/plain':
-                pattern = r'/auth/verify\?token=token'
-                match = re.search(pattern, part.get_payload(decode=True).decode('utf-8'))
-                assert match is not None
-                break
+class TestMailTemplate:
 
+    def test_verification_to_html(self):
+        settings = Settings()
+        template = MailTemplate('Subject', 'verify', {'token': 'jwt_token'})
+        html = template.to_html()
+        assert 'confirm email' in html
+        assert f'{settings.service_public_address}/auth/verify?token=jwt_token' in html
+        assert template.subject == 'Subject'
 
-def test_send_reset_password_email():
-    with SmtpMockServer('127.0.0.1', 42000) as srv:
-        send_reset_password_email(TEST_SEND_EMAIL, 'token')
-        message: Message = srv.messages[0]
-        for part in message.walk():
-            if part.get_content_type() == 'text/plain':
-                pattern = r'/auth/reset-password\?token=token'
-                match = re.search(pattern, part.get_payload(decode=True).decode('utf-8'))
-                assert match is not None
-                break
+    def test_reset_to_html(self):
+        settings = Settings()
+        template = MailTemplate('Subject', 'reset', {'token': 'jwt_token'})
+        html = template.to_html()
+        assert 'reset password' in html
+        assert f'{settings.service_public_address}/auth/reset-password?token=jwt_token' in html
+        assert template.subject == 'Subject'
