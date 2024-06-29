2020d4c5167f6f2826c431018cafc744fdf9df84
Louke <git@jannsen.org>
2024-06-29 16:40:00 +0200

    Remove unused import

--- itzmenu-service/src/itzmenu_service/config/settings.py
+++ itzmenu-service/src/itzmenu_service/config/settings.py
@@ -1,5 +1,5 @@
 from pydantic import Field
-from pydantic_settings import BaseSettings, SettingsConfigDict, PydanticBaseSettingsSource
+from pydantic_settings import BaseSettings, SettingsConfigDict
 import re
 
 
